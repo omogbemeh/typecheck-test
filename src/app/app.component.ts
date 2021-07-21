3cb88100d5e61c2be46d03b437911dad603820fe
omogbemeh <onosithena@gmail.com>
2021-07-21 22:37:38 +0100

    Final Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'test-project';
+export class AppComponent implements OnInit {
+  sentence = lorem.sentence().slice(0, -1);
+  input = '';
+  onChange(e: string, value: HTMLElement) {
+    this.input = e;
+  }
+  ngOnInit() {
+    this.sentence = lorem.sentence().slice(0, -1);
+  }
+  getRandomWord() {
+    this.sentence = lorem.sentence().slice(0, -1);
+  }
+  compare(letter: string, i: number) {
+    if (!this.input[i]) return 'pending';
+    return letter != this.input[i] ? 'error' : 'success';
+  }
 }
